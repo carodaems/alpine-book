10b87d1a8fbd0f945f2531babbab71d19274ea80
carodaems <r0889345@student.thomasmore.be>
2022-11-16 21:04:16 +0100

    added styling with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./test.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['"Poppins"', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
